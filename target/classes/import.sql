527a575e1bb8ad397ec39605da03668bc975526e
elverjbeltran <elverbeltran04@gmail.com>
2024-06-04 12:29:06 -0500

    update target

---
+++ target/classes/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO libro (autor, categoria, disponibilidad, isbn, titulo) VALUES
+('Gabriel García Márquez', 'Novela', true, '978-3-16-148410-0', 'Cien Años de Soledad'),
+('Julio Cortázar', 'Cuento', true, '978-0-7432-7356-5', 'Rayuela'),
+('Mario Vargas Llosa', 'Novela', true, '978-0-7432-7356-6', 'La ciudad y los perros');
